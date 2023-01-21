882c2c96aa095235b923e25cc0a8f906c18c861b
JaniHarkonen <jani.harkonen.real@gmail.com>
2023-01-21 18:23:47 +0200

    Installed bootstrap and react-bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import BootstrapTest from "./components/BootstrapTest";
+
+const test_article = {
+  articleTitle: "FINNAIR DID 9/11 - stock soars 911%",
+  articleLink: "www.pornhub.com",
+  publishDate: "11/09/2001",
+  readDate: "12/12/2012",
+  tags: [
+    "coronavirus",
+    "inflation",
+    "oil",
+    "crisis",
+    "boooooooooriiiiiiinnnnnnnng",
+    "coronavirus",
+    "inflation",
+    "oil",
+    "crisis",
+    "boooooooooriiiiiiinnnnnnnng",
+    "coronavirus",
+    "inflation",
+    "oil",
+    "crisis",
+    "boooooooooriiiiiiinnnnnnnng"
+  ],
+  notes: "hmm, what an interesting article\n->indeed, indubitably it was\nchange my mind"
+};
 
 
 function App() {
   return (
     <div className="App">
-      test
+      <BootstrapTest article={test_article} />
     </div>
   );
 }
