20b6a0493199116bc5525fb3ae5ca87a2fa8f6c6
JaniHarkonen <jani.harkonen.real@gmail.com>
2023-02-09 20:38:04 +0200

    Added ability to edit tags.

--- src/components/TagList/TagList.js
+++ src/components/TagList/TagList.js
@@ -1,51 +1,38 @@
 import { useContext, Fragment } from "react";
 import { GlobalContext } from "../../context/GlobalContext";
 import ArticleTag from "../ArticleTag/ArticleTag";
+import FormModal from "../../modal/FormModal/FormModal";
+import createTagForm from "../../modal/createTagForm";
+import useTagForm from "../../hooks/useTagForm";
 
-
-const TEST_TAGS = [
-  {
-    id: 1,
-    name: "coronavirus",
-    color: { r: 255, g: 0, b: 0 }
-  },
-  {
-    id: 2,
-    name: "inflation",
-    color: { r: 0, g: 255, b: 0 }
-  },
-  {
-    id: 3,
-    name: "crypto",
-    color: { r: 0, g: 0, b: 255 }
-  },
-  {
-    id: 4,
-    name: "oil",
-    color: { r: 128, g: 128, b: 128 }
-  }
-];
 
 export default function TagList() {
-  const { workspaceManager: ws } = useContext(GlobalContext);
+  const { popupModal, workspaceManager: ws, languageManager: lm } = useContext(GlobalContext);
 
-  const renderTagListings = () => {
-    return ws.getTagContainer().mapItems((tag) => {
-      return (
-        <Fragment key={"tag-list-article-tag" + tag.tagId}>
-          <ArticleTag
-            name={tag.name}
-            color={tag.color}
-          />
-          <br />
-        </Fragment>
-      );
-    });
+  const handleTagEdit = (tag) => {
+    const form = createTagForm(tag, lm);
+    
+    popupModal(
+      <FormModal
+        title={form.title}
+        form={form.form}
+        footer={form.footer}
+        useForm={useTagForm}
+        baseInstance={tag}
+      />
+    );
   };
 
-  return (
-    <>
-      {renderTagListings()}
-    </>
-  );
+  return ws.getTagContainer().mapItems((tag) => {
+    return (
+      <Fragment key={"tag-list-article-tag" + tag.tagId}>
+        <ArticleTag
+          name={tag.name}
+          color={tag.color}
+          onClick={() => handleTagEdit(tag)}
+        />
+        <br />
+      </Fragment>
+    );
+  });
 }
