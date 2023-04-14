7ac037df30a5f998651e73a65d59d19f03df8020
JaniHarkonen <jani.harkonen.real@gmail.com>
2023-04-15 00:05:14 +0300

    Tags are now assembled using inline-block.

--- src/components/TagList/TagList.js
+++ src/components/TagList/TagList.js
@@ -1,28 +1,44 @@
 import { useContext, Fragment } from "react";
-import { GlobalContext } from "../../context/GlobalContext";
+
 import ArticleTag from "../ArticleTag/ArticleTag";
 import useFormModal from "../../hooks/modal/useFormModal";
 import createTagPopup from "../../modals/create/tag/createTagPopup"
 
+import { GlobalContext } from "../../context/GlobalContext";
+import { Styles } from "./TagList.styles";
+
+
+export const ELEMENT_ID = {
+  tagSizeContainer: "tag-size-container"
+};
 
 export default function TagList() {
   const { workspaceManager: ws } = useContext(GlobalContext);
   const { popup } = useFormModal();
+  const tagContainer = ws.getTagContainer();
 
   const handleTagEdit = (tag) => {
     popup(createTagPopup(tag));
   };
 
-  return ws.getTagContainer().mapItems((tag) => {
-    return (
-      <Fragment key={"tag-list-article-tag" + tag.tagId}>
-        <ArticleTag
-          name={tag.name}
-          color={tag.color}
-          onClick={() => handleTagEdit(tag)}
-        />
-        <br />
-      </Fragment>
-    );
-  });
+  const renderTags = () => {
+    return tagContainer.mapItems((tag) => {
+      return (
+        <Styles.TagContainer key={"tag-list-article-tag-" + tag.tagId}>
+          <ArticleTag
+            name={tag.name}
+            color={tag.color}
+            onClick={() => handleTagEdit(tag)}
+          />
+          <br />
+        </Styles.TagContainer>
+      );
+    });
+  };
+
+  return (
+    <Styles.Content>
+      {renderTags()}
+    </Styles.Content>
+  );
 }
---
+++ src/components/TagList/TagList.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+const Content = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const TagContainer = styled.div`
+  display: inline-block;
+`;
+
+export const Styles = {
+  Content,
+  TagContainer
+};
